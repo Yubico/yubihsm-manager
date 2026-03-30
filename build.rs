c4f5c68dd3987cf1853438473d7d163571e0dfe6
Aveen Ismail <aveen.ismail@yubico.com>
2026-03-30 15:10:06 +0200

    Release: link statically to dependencied in MacOS

--- build.rs
+++ build.rs
@@ -7,5 +7,18 @@ fn main() {
     #[cfg(target_os = "macos")]
     {
         println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/../lib");
+
+        // libyubihsm.a (static) has unresolved symbols for curl and libusb.
+        // These will be satisfied by:
+        //   - libcurl: system dylib (ships with macOS)
+        //   - libusb:  bundled dylib in ../lib/
+        println!("cargo:rustc-link-lib=curl");
+        println!("cargo:rustc-link-lib=usb-1.0");
+
+        // Where to find libusb at build time
+        println!("cargo:rerun-if-env-changed=BREW_LIB");
+        if let Ok(brew_lib) = std::env::var("BREW_LIB") {
+            println!("cargo:rustc-link-search={}/libusb/lib", brew_lib);
+        }
     }
 }
